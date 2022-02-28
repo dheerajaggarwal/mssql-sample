e4662dd57b70a3851111e6d565a3a76348eed171
Dheeraj Aggarwal <mail.dheerajaggarwal@gmail.com>
2022-02-28 16:28:37 +0530

    config.js issue fixed

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "query": `select * from mytable where id = ${value}`,
+  "query": `select * from mytable where id = db_id`,
   "hostname": "localhost",
   "port": 1433,
   "database": "db_name",

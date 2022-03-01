9c2e1dc3353d48d6f867d30a278e43491c103e34
Dheeraj Aggarwal <mail.dheerajaggarwal@gmail.com>
2022-03-01 12:13:17 +0530

    native driver of mssql added for kerberos authentication

--- config.js
+++ config.js
@@ -1,9 +1,7 @@
 //TODO: define these parameters as per your database configuration.
 module.exports = {
   "query": `select * from mytable where id = db_id`,
-  "hostname": "localhost",
+  "server": "SERVER\\NAME",
   "port": 1433,
-  "database": "db_name",
-  "username": "username",
-  "password": "password"
+  "database": "db_name"
 }
--- index.js
+++ index.js
@@ -1,31 +1,25 @@
-const sql = require('mssql');
+const sql = require('mssql/msnodesqlv8');
 const obj = require('./config');
 
 
 async function getDBConnection(){
-  try {
-    const sqlConfig = {
-      user: obj.username,
-      password: obj.password,
-      database: obj.database,
-      server: obj.hostname,
-      port: obj.port,
-      pool: {
-        max: 10,
-        min: 0,
-        idleTimeoutMillis: 30000
-      },
-      options: {
-        encrypt: true, // for azure
-        trustServerCertificate: false // change to true for local dev / self-signed certs
-      }
+  const sqlConfig = {
+    database: obj.database,
+    server: obj.hostname,
+    driver: "msnodesqlv8",
+    port: obj.port,
+    pool: {
+      max: 10,
+      min: 0,
+      idleTimeoutMillis: 30000
+    },
+    options: {
+      trustedConnection: true,
+      trustServerCertificate: false // change to true for local dev / self-signed certs
     }
-    await sql.connect(sqlConfig);
-    console.log("connection successful...");
-  } catch(error){
-    console.log("Connection Failure!!!");
-    console.error(error);
   }
+  await sql.connect(sqlConfig);
+  console.log("connection successful...");
 }
 
 async function executeQuery(query){
@@ -34,8 +28,12 @@ async function executeQuery(query){
 }
 
 async function mainFunction(){
-  await getDBConnection();
-  await executeQuery(obj.query);
+  try {
+    await getDBConnection();
+    await executeQuery(obj.query);
+  } catch(err){
+    console.error(err);
+  }
 }
 
 mainFunction();
